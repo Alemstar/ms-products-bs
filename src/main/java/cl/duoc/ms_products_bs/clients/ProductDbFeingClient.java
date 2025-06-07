4e74fc8fb17438047b9fd794ba945c0aef9f6c04
Plaza Vespucio <plazavespucio@PVAWSB03LC1006.duoc.acad>
2025-06-07 13:54:31 -0400

    v.2

--- src/main/java/cl/duoc/ms_products_bs/clients/ProductDbFeingClient.java
+++ src/main/java/cl/duoc/ms_products_bs/clients/ProductDbFeingClient.java
@@ -1,5 +1,14 @@
 package cl.duoc.ms_products_bs.clients;
 
-public interface ProductDbFeingClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import cl.duoc.ms_products_bs.model.ProductDTO;
+
+@FeignClient(name="ms-products-db", url = "http://localhost:8280/api/products")
+public interface ProductDbFeingClient {
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findProductById(@PathVariable(name = "idProduct") Long idProduct);
 }
